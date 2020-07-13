f31e63cd6ea42ac85b68f4db5be8a5226e1cdb4b
Matheus <raydra10@gmail.com>
2020-07-13 17:34:40 -0300

    Commit on Git

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './styles.css';
+
+import React from 'react';
+import Header from './components/Header';
+import Main from './pages/main'
+import Routes from './routes'
+
+
+
+const App = () => (
+    <div className="App">
+      <Header />
+      <Routes />
+    </div>
+  
+);
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => <header id="main-header">JsHunt</header>
+
+export default Header;
